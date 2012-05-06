e0f388cdf309437ddc0e76ed7d934cde4dec6b60
Yoshi <yoshi@44services.jp>
2012-05-06 10:58:25 +0900

    fix tokens

--- HttpPageFlowBundle/Tests/Delegate/TestDelegator.php
+++ HttpPageFlowBundle/Tests/Delegate/TestDelegator.php
@@ -3,14 +3,15 @@
  *
  * Description:
  *      
- * $Id$
- * $Date$
- * $Rev$
- * $Author$
  * 
- *  This file is part of the $Project$ package.
+ * $Date$
+ * Rev    : see git
+ * Author : Yoshi Aoki <yoshi@44services.jp>
+ * 
+ *  This file is part of the Rock package.
  *
- * $Copyrights$
+ * For the full copyright and license information, 
+ * please read the LICENSE file that is distributed with the source code.
  *
  ****/
 // @namespace
